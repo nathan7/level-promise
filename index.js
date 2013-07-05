a7395d1a6781d4e94c4260545efb08e75dad6f0e
Nathan Zadoks <nathan@nathan7.eu>
2013-07-05 14:00:08 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var Manifest = require('level-manifest')
+  , Promise = require('promise')
+  , __hop = {}.hasOwnProperty
+
+exports .install = install
+function install(db) {
+  var manifest = Manifest(db)
+    , methods = manifest.methods
+
+  for (var methodName in methods) if (__hop.call(methods, methodName)) {
+    var method = methods[methodName]
+    if (method.type === 'async')
+      substitute(methodName, method)
+  }
+
+  function substitute(methodName, method){
+    var methodP = Promise.denodeify(method)
+    db[methodName] = function() {
+      if (typeof arguments[arguments.length - 1] == 'function')
+        method.apply(this, arguments)
+      else
+        return methodP.apply(this, arguments)
+    }
+  }
+  
+  return db
+}
